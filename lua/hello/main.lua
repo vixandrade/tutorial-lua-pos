690752d321d6b565aacc9ca390b15645a58dfee2
vixandrade <vix.andrade21@gmail.com>
2016-07-17 23:04:29 -0300

    Primeira versão com menu

---
+++ lua/hello/main.lua
@@ -0,0 +1,30 @@
+function getQuote()
+	return "Try not. Do."
+end
+
+vix = {
+	name = "Victor de Andrade",
+	age = 22,
+	quote = getQuote()
+}
+
+function main()
+	menu = ui.menu("Imprimir:", {"Nome","Idade","Frase","Tudo"})
+	menu:show(20)
+	
+	op = menu:accepted()
+	
+	if op == 1 then
+		printer.print(vix.name)
+	elseif op == 2 then
+		printer.print(vix.age)
+	elseif op == 3 then	
+		printer.print(vix.quote)
+	elseif op == 4 then
+		printer.print(vix.name)
+		printer.print(vix.age)
+		printer.print(vix.quote)
+	else
+		return
+	end
+end
