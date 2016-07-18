461908779899ecd50ec9575a2fdc2e04a39128b5
vixandrade <vix.andrade21@gmail.com>
2016-07-17 23:11:27 -0300

    Terceira versão com valores exibidos na tela

--- lua/hello/main.lua
+++ lua/hello/main.lua
@@ -11,7 +11,7 @@ vix = {
 }
 
 function main()
-	menu = ui.menu("Imprimir:", {"Nome","Idade","Frase","Tudo"})
+	menu = ui.menu("Imprimir:", {"Nome","Idade","Frase","Tudo", "Ver", "Ver Pausado"})
 	menu:show(20)
 	
 	op = menu:accepted()
@@ -24,6 +24,17 @@ function main()
 		printer.print(vix.quote)
 	elseif op == 4 then
 		hutil.fPrintTable(vix)
+	elseif op == 5 then
+		ui.transient("Nome",vix.name,3000)
+		ui.transient("Idade",vix.age,3000)
+		ui.transient("Frase",vix.quote,3000)
+	elseif op == 6 then
+		ui.message("Nome",vix.name)
+		keyboard.getkeystroke()
+		ui.message("Idade",vix.age)
+		keyboard.getkeystroke()
+		ui.message("Frase",vix.quote)
+		keyboard.getkeystroke()
 	else
 		return
 	end
